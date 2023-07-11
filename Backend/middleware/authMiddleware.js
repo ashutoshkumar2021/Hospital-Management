94536b58a4b5f28f24ce16bbbbe04a8c0f6e3b88
Ashutosh Kumar <80633589+ashutoshkumar2021@users.noreply.github.com>
2023-07-12 02:17:43 +0530

    Update authMiddleware.js

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -1,6 +1,14 @@
 const JWT=require ('jsonwebtoken')
+
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}
 module.exports=async(req,res,next)=>{
     try{
+        allowCrossDomain(req, res, () => {});
         const token=req.headers["authorization"].split(" ")[1]
     JWT.verify(token,process.env.JWT_SECRET,(err,decode)=>{
         if(err){
