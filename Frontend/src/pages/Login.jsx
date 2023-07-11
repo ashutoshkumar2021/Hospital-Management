4f3a3ee038e896d7b72c8bef80a7295b39ff5804
Ashutosh Kumar <80633589+ashutoshkumar2021@users.noreply.github.com>
2023-07-12 01:27:52 +0530

    Update Login.jsx

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -14,7 +14,7 @@ const Login = () => {
   const onFinishHandler = async (values) => {
     try {
       dispatch(showLoading())
-      const res = await axios.post("/api/v1/user/login", values);
+      const res = await axios.post("https://hospital-management-veiu.onrender.com/api/v1/user/login", values);
       window.location.reload();
       dispatch(hideLoading())
       if (res.data.success) {
